a287f971da623b71fa6177f5ce7a3fa3b350c55a
Aniket <anikethumane777@gmail.com>
2023-05-10 23:13:51 +0530

    Create new Project

---
+++ AkshayDemo/src/com/ash/DemoClass.java
@@ -0,0 +1,18 @@
+package com.ash;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+	
+		int x = 5;
+		int y = 10;
+		
+		x= x+y;
+		y=x-y;
+		x=x-y;
+		
+		System.out.println("Y is>>>"+y);
+		System.out.println("x is>>>"+x);
+	}
+
+}
